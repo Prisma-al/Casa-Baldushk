8ec6dc873747f739ac43c3f867d7de05744ffbeb
Fabbb16 <stafasanifabio2@gmail.com>
2026-07-22 12:53:48 +0200

    added draft state to fix the pos error

--- profisc_custom_changes_casa/models/pos_order.py
+++ profisc_custom_changes_casa/models/pos_order.py
@@ -10,15 +10,21 @@ class PosOrder(models.Model):
     def _load_pos_data_fields(self, config_id):
         fields_list = super()._load_pos_data_fields(config_id)
         if "is_draft" not in fields_list:
-            fields_list['is_draft'] = 'is_draft'
+            fields_list.append("is_draft")
         return fields_list
+
+    @api.model
+    def _order_fields(self, ui_order):
+        vals = super()._order_fields(ui_order)
+        if ui_order.get("is_draft"):
+            vals["state"] = "draft"
+            vals["to_invoice"] = False
+        return vals
 
     def _process_order(self, order, existing_order):
         order_id = super()._process_order(order, existing_order)
 
         data = order.get("data", order) if isinstance(order, dict) else {}
         if data.get("is_draft"):
-            pos_order = self.browse(order_id)
-
-            pos_order.write({"state": "draft", "to_invoice": False})
+            self.browse(order_id).write({"state": "draft", "to_invoice": False})
         return order_id
