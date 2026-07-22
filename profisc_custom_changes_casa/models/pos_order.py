a6da114374b46ae1ab4fee5bce65d72038152260
Fabbb16 <stafasanifabio2@gmail.com>
2026-07-22 13:03:31 +0200

    added draft state to fix the pos error 2

--- profisc_custom_changes_casa/models/pos_order.py
+++ profisc_custom_changes_casa/models/pos_order.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
-    is_draft = fields.Boolean(string="POS Draft", default=False)
+    is_draft = fields.Boolean(string="Skip Fiscalization (Draft)", default=False)
 
     @api.model
     def _load_pos_data_fields(self, config_id):
@@ -12,19 +12,3 @@ class PosOrder(models.Model):
         if "is_draft" not in fields_list:
             fields_list.append("is_draft")
         return fields_list
-
-    @api.model
-    def _order_fields(self, ui_order):
-        vals = super()._order_fields(ui_order)
-        if ui_order.get("is_draft"):
-            vals["state"] = "draft"
-            vals["to_invoice"] = False
-        return vals
-
-    def _process_order(self, order, existing_order):
-        order_id = super()._process_order(order, existing_order)
-
-        data = order.get("data", order) if isinstance(order, dict) else {}
-        if data.get("is_draft"):
-            self.browse(order_id).write({"state": "draft", "to_invoice": False})
-        return order_id
